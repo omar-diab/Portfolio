cccd169097460c10d1e1c6ed049839a25f8dbd5b
OMAR <omar.i.s.diab@gmail.com>
2025-07-28 02:35:19 +0300

    Edit Project Link

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -4,7 +4,7 @@ export const projects = [
     title: "To Do List",
     description: "Built with Reactjs",
     tools: ["Reactjs", "Vite", "MUI", "TailwindCss"],
-    link: "https://to-do-list-self-psi-41.vercel.app/",
+    link: "https://to-do-lisl.vercel.app/",
     image: "/assets/projects/to-do-list.png",
     categories: ["Reactjs"],
   },
